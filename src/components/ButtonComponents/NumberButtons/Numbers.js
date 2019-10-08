edce5a20016317289d2e8b808ac7311393cf8c32
Michael Nunes <miketnunes@gmail.com>
2019-10-08 12:30:32 -0700

    Import modules from datajs to components

--- src/components/ButtonComponents/NumberButtons/Numbers.js
+++ src/components/ButtonComponents/NumberButtons/Numbers.js
@@ -6,6 +6,7 @@ import React from "react";
 import { numbers } from '../../../data' 
 */
 //Import your array data to from the provided data file
+import { numbers } from '../../../data';
 
 const Numbers = () => {
   // STEP 2 - add the imported data to state
