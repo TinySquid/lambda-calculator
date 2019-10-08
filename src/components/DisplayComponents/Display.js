6de162197d6de2ba9d3d038687077bf8f2b57ade
Michael Nunes <miketnunes@gmail.com>
2019-10-08 16:36:02 -0700

    Implement Display component

--- src/components/DisplayComponents/Display.js
+++ src/components/DisplayComponents/Display.js
@@ -1,5 +1,9 @@
 import React from "react";
 
-const Display = () => {
-  return <div className="display">{/* Display any props data here */}</div>;
+const Display = (props) => {
+  const number = props.value;
+
+  return <div className="display">{number}</div>;
 };
+
+export default Display;
